308989b2ef5d60e741ddd359c55f01b2768ebe49
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-02-11 19:59:51 +0300

    Объекты

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        book book = new book();
+        System.out.println(book);
+
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,5 @@
+public class Book {
+    String Author;
+    String  NameBook;
+    int  YearBook;
+}
