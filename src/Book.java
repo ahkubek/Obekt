fe748f52a8704bb7a992a9a1392ef43c47b52358
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-02-26 13:03:13 +0300

    ObjectFinal

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 private String title;
 private int publisheryear;
@@ -31,5 +33,23 @@ private Author author;
         this.title = title;
         this.publisheryear = publisheryear;
         this.author = author;
+    }
+
+    @Override
+    public String toString() {
+  return "Название книги: "+this.title+" год публикации "+this.publisheryear+"Автор "+this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publisheryear == book.publisheryear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, publisheryear, author);
     }
 }
